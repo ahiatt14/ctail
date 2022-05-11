7f7ef56e7a74c2e07a334d4fd8b188380afaf845
Alex Hiatt <alexryanhiatt@gmail.com>
2022-05-11 13:36:16 -0500

    added glfw glad loading to window creation fn

--- src/glfw_window.c
+++ src/glfw_window.c
@@ -1,5 +1,6 @@
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
+#include "glad.h"
 
 #include "window.h"
 
@@ -107,6 +108,7 @@ const window_api* window__create(
   glfwSetWindowRefreshCallback(glfw_window, glfwSwapBuffers);
 
   glfwMakeContextCurrent(glfw_window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return NULL;
 
   glfwSwapInterval(vsync);
 
